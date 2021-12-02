0a92e21c0c9d3b3094824e447f4da8090c48aa1f
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2021-12-02 21:48:00 +0600

    Update A.cpp

--- Education_Round/Round118/A.cpp
+++ Education_Round/Round118/A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using ll = long long int;
 int main(){
 	ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+    	cin.tie(0);cout.tie(0);
 	int test;cin >> test;
 	while(test--){
 		string x1,x2;
